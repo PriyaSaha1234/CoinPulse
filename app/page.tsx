7194ace4d2d2d86e0ed71266fc0d3ecc4c8dc441
Priya Saha <priyasahasawhney2223@gmail.com>
2026-07-19 00:36:24 +0530

    implement various features

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, {Suspense} from 'react'
+import CoinOverview from "@/components/home/CoinOverview";
+import TrendingCoins from "@/components/home/TrendingCoins";
 
-const Page= () =>{
-  return <div></div>
+const Page= async () =>{
+  return <main className="main-container">
+    <section className="home-grid">
+        <Suspense fallback={<div>Loading...</div>}>
+            <CoinOverview />
+        </Suspense>
+
+        <Suspense fallback={<div>Loading Trending...</div>}>
+            <TrendingCoins />
+        </Suspense>
+
+    </section>
+
+    <section className="w-full mt-7 space-y-4">
+      <p>Categories</p>
+    </section>
+  </main>
 }
 export default Page
